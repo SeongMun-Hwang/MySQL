c2f73f2854779463f59f37dcd9430992f745a2e9
SeongMun-Hwang <hwang105814@gmail.com>
2022-12-02 00:00:09 +0900

    add 1 main menu function, make exception handling

--- mysql.py
+++ mysql.py
@@ -43,7 +43,7 @@ def check_character_all(name, weapon, relic):
         return 0
     
 while True :
-    print("1. 유저   2. 캐릭터   3.종료")
+    print("1. 유저   2. 캐릭터   3.종료 \n4.캐릭터,무기,성유물 조회")
     func=input("사용할 기능을 선택하세요: ")
     #유저 기능 선택
     if func == '1':
@@ -60,15 +60,23 @@ while True :
             
             #유저 등록
             if user_num == '1':
-                uid, nickname, gender=input("유아이디, 닉네임, 성별을 입력하세요: ").split(",")
+                while True:
+                    uid, nickname, gender=input("유아이디, 닉네임, 성별을 입력하세요: ").split(",")
+                    if check_exist('사용자', '유아이디', uid)==0:
+                        break
+                    else :
+                        print("이미 등록된 사용자입니다")
+                
                 sql="INSERT INTO 사용자 (유아이디, 닉네임, 성별) VALUE ("+uid+",'"+nickname+"','"+gender+"');"
                 execute_sql(sql)
                 
             #유저 정보 수정
-            elif user_num == '2':
-                uid=input("사용자 유아이디를 입력해 주세요: ")
-                sql="SELECT * FROM 사용자 WHERE 유아이디='"+uid+"';"
-                execute_sql(sql)
+            elif user_num == '2':                   
+                while True:
+                    uid=input("수정할 사용자 유아이디를 입력해 주세요: ")
+                    if check_exist('사용자', '유아이디', uid) !=0:
+                        break
+                
                 nickname, gender=input("수정할 닉네임과 성별을 입력해주세요: ").split(",")
                 sql="UPDATE 사용자 SET 닉네임='"+nickname+"',성별='"+gender+"' WHERE 유아이디='"+uid+"';"
                 execute_sql(sql)
@@ -80,12 +88,19 @@ while True :
                 
             #유저 삭제
             elif user_num == '4':
-                uid=input("삭제하고자 하는 유아이디를 입력하세요: ")
+                while True:
+                    uid=input("삭제하고자 하는 유아이디를 입력하세요: ")
+                    if check_exist('사용자', '유아이디', uid) !=0:
+                        break
+                
                 sql="DELETE FROM 사용자 WHERE 유아이디 ='"+uid+"';"
                 execute_sql(sql)
                 
             elif user_num == '5':
                 break
+            
+            else :
+                print("잘못된 입력입니다.\n")
         
     #캐릭터 기능 선택
     elif func == '2':     
@@ -95,6 +110,7 @@ while True :
                 break
        
         while True:           
+            print("\n현재 유아이디:",uid)
             print("캐릭터 메뉴")
             print("1. 보유 캐릭터 등록")
             print("2. 보유 캐릭터 수정")
@@ -104,21 +120,24 @@ while True :
             print("!입력이 여러 개일시 ,를 사용하세요!")
             char_num=input("원하시는 기능을 선택하세요: ")
             print("\n")
+            
             #보유 캐릭터 등록
             if char_num == '1':
-                character, level, weapon, relic=input("추가하려는 캐릭터, 레벨, 무기, 성유물 이름을 입력해주세요: ").split(',')
-                sql="INSERT 보유_캐릭터 SET 유아이디="+uid+",캐릭터_이름='"+character+"',레벨="+level+",무기_이름='"+weapon+"',성유물_이름='"+relic+"';"
-                if check_character_all(character, weapon, relic)==0:
-                    break
+                while True:
+                    character, level, weapon, relic=input("추가하려는 캐릭터, 레벨, 무기, 성유물 이름을 입력해주세요: ").split(',')
+                    sql="INSERT 보유_캐릭터 SET 유아이디="+uid+",캐릭터_이름='"+character+"',레벨="+level+",무기_이름='"+weapon+"',성유물_이름='"+relic+"';"
+                    if check_character_all(character, weapon, relic)!=0:
+                        break              
                 execute_sql(sql)
                 cal_status(character)
                 
             #보유 캐릭터 수정
             elif char_num == '2':
-                character, weapon, relic=input("수정할 캐릭터, 레벨, 무기, 성유물 이름을 입력해주세요: ").split(',')
-                sql="UPDATE 보유_캐릭터 SET 무기_이름='"+weapon+"',레벨="+level+"',성유물_이름='"+relic+"' WHERE 유아이디="+uid+" AND 캐릭터_이름='"+character+"';"
-                if check_character_all(character, weapon, relic)==0:
-                    break
+                while True:
+                    character, level, weapon, relic=input("수정할 캐릭터, 레벨, 무기, 성유물 이름을 입력해주세요: ").split(',')
+                    sql="UPDATE 보유_캐릭터 SET 무기_이름='"+weapon+"',레벨="+level+",성유물_이름='"+relic+"' WHERE 유아이디="+uid+" AND 캐릭터_이름='"+character+"';"
+                    if check_character_all(character, weapon, relic)!=0:
+                        break
                 execute_sql(sql)
                 cal_status(character)
                 
@@ -129,16 +148,33 @@ while True :
                 
             #보유 캐릭터 삭제
             elif char_num == '4':
-                character = input("삭제할 캐릭터 이름을 입력해 주세요: ")
+                while True:
+                    character = input("삭제할 캐릭터 이름을 입력해 주세요: ")
+                    sql="SELECT * FROM 보유_캐릭터 WHERE EXISTS(SELECT * FROM 보유_캐릭터 WHERE 캐릭터_이름='"+character+"' AND 유아이디="+uid+");"
+                    if cur.execute(sql) != 0:
+                        break
+                    else :
+                        print("해당 유아이디 계정에 존재하지 않는 캐릭터입니다.\n")
                 sql="DELETE FROM 보유_캐릭터 WHERE 유아이디="+uid+" AND 캐릭터_이름='"+character+"';"
                 execute_sql(sql)                
                 
             elif char_num == '5':
-                break                
+                break
+            
+            else :
+                print("잘못된 입력입니다.\n")                
                       
     elif func == '3':
         print("bye")
         break
+    
+    elif func== '4':
+        sql="SELECT 캐릭터_이름 FROM 캐릭터"
+        execute_sql(sql)
+        sql="SELECT 무기_이름 FROM 무기"
+        execute_sql(sql)
+        sql="SELECT 성유물_이름 FROM 성유물"
+        execute_sql(sql)
                     
 con.close()
 
